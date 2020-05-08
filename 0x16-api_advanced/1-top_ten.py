9a3ed866db6c92a81b421517c8b33f0900f09ab9
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-05-07 23:55:58 -0500

    Added task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Queries the Reddit API and prints the titles of the first 10 hot posts
+listed for a given subreddit. """
+import json
+import requests
+
+
+def top_ten(subreddit):
+    """ top ten """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'linux:com.myapp:v.1'}
+    params = {'limit': 10}
+    res = requests.get(url=url, headers=headers, params=params)
+
+    if res.status_code == 404:
+        print('None')
+        return
+
+    list = res.json().get('data').get('children')
+    for post in list:
+        print(post.get('data').get('title'))
