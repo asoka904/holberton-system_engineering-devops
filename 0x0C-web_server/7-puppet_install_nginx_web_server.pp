60c6a755091b99240deb58c4910b7b249717216a
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-08-07 20:42:01 -0500

    Added task 6

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,1 +1,24 @@
-#!/usr/bin/env bash
+# 6. Install Nginx web server (w/ Puppet)
+
+package { 'nginx':
+  ensure => installed,
+  name   => 'nginx',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Holberton School',
+  path    => '/var/www/html/index.html'
+}
+
+file_line { 'title':
+  ensure   => present,
+  path     => '/etc/nginx/sites-available/default',
+  after    => 'server_name _;',
+  line     => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;',
+  multiple => true
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
