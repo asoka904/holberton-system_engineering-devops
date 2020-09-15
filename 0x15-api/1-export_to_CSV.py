d89d26a807251d07d712dc5b2174763309d73fff
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-09-14 23:50:53 -0500

    Added task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python3
+"""
+    Using this REST API (https://jsonplaceholder.typicode.com/), for a given
+    employee ID, returns information about his/her TODO list progress.
+"""
+import csv
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    req = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                       format(argv[1]))
+    name = req.json().get('name')
+    req = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.
+                       format(argv[1]))
+
+    data = req.json()
+    done = 0
+    total = 0
+
+    with open('{}.csv'.format(argv[1]), mode='w') as data_file:
+        csv_writer = csv.writer(data_file, delimiter=',', quotechar='"',
+                                quoting=csv.QUOTE_ALL)
+
+    for task in data:
+        total += 1
+        if task.get('completed'):
+            done += 1
+    print('Employee {} is done with tasks({}/{}):'.format(name, done, total))
+
+    for task in data:
+        csv_writer.writerow([argv[1], username, task.get('completed'),
+                            task.get('title')])
