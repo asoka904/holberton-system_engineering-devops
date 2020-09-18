69b9fd780198f41eebe53455668a4d7639a5fea3
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-09-17 23:53:02 -0500

    Added task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""
+Queries the Reddit API and prints the titles of the first 10 hot posts listed
+for a given subreddit.
+"""
+import json
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    """ recursively """
+    if after is None:
+        return []
+
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'python3'}
+    params = {'limit': 100, 'after': after}
+    res = requests.get(url=url, headers=headers, params=params)
+    if res.status_code == 404:
+        return None
+
+    res = res.json()
+    for post in res.get('data').get('children'):
+        hot_list.append(post.get('data').get('title'))
+    after = res.get('data').get('after')
+    return recurse(subreddit, hot_list, after)
