3c7a1961d579b6f18f2b52643ee3a0a440c84d90
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-09-17 23:22:37 -0500

    Added task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+Returns the number of subscribers for a given subreddit.
+"""
+import json
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ Get request obtain suscribers """
+    headers = {'User-Agent': 'python3'}
+
+    response = requests.get('https://www.reddit.com/r/{}/about'.
+                            format(subreddit), headers=headers)
+
+    if response.status_code != 200:
+        return 0
+
+    response = requests.get('https://www.reddit.com/r/{}/about.json'.
+                              format(subreddit), headers=headers).json()
+
+    return response.get('data').get('subscribers')
