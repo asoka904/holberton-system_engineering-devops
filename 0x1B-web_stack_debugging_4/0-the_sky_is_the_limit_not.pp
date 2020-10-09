4ee3eebd1afe549cbe470469f331ad54ef7b302b
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-10-08 21:21:44 -0500

    Added task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,1 +1,5 @@
-#!/usr/bin/env bash
+# Increase limit
+exec { 'Increase_workers':
+  command  => "sed -i 's/worker_processes 4;/worker_processes 100;/g' /etc/nginx/nginx.conf && service nginx restart",
+  provider => 'shell',
+}
